5326aa0890cdc38dc3d98d7d0de166ff45e5d28e
Harald-LB <Harald-lb@free-creations.de>
2020-08-17 16:55:25 +0200

    Event receiver added.

--- tests/unit_tests/alsa_helper.cpp
+++ tests/unit_tests/alsa_helper.cpp
@@ -40,23 +40,50 @@ void AlsaHelper::checkAlsa(const char *operation, int alsaResult) {
   }
 }
 
+snd_seq_t * AlsaHelper::hSequencer{nullptr}; /// handle to access the ALSA sequencer
+int AlsaHelper::clientId{0}; /// the client-number of this client
+struct pollfd * AlsaHelper::pPollDescriptor{nullptr};
+int AlsaHelper::pollDescriptorsCount{0};
+
 /**
  * Open the ALSA sequencer in non-blocking mode.
  */
 void AlsaHelper::openAlsaSequencer() {
   int err;
   // open sequencer (we need a duplex stream, in order to start and stop the queue).
-  err = snd_seq_open(&_hSequencer, "default", SND_SEQ_OPEN_DUPLEX, SND_SEQ_NONBLOCK);
+  err = snd_seq_open(&hSequencer, "default", SND_SEQ_OPEN_DUPLEX, SND_SEQ_NONBLOCK);
   checkAlsa("open sequencer", err);
 
   // set our client's name
-  err = snd_seq_set_client_name(_hSequencer, "a_j_midi-tests");
+  err = snd_seq_set_client_name(hSequencer, "a_j_midi-tests");
   checkAlsa("set client name", err);
 
-  _clientId = snd_seq_client_id(_hSequencer);
-  spdlog::trace("Alsa client {} created.", _clientId);
+  clientId = snd_seq_client_id(hSequencer);
 
+  // lets create the poll descriptor that we will need when we wait for incoming events.
+  pollDescriptorsCount = snd_seq_poll_descriptors_count(hSequencer, POLLIN);
+  pPollDescriptor = (struct pollfd *) alloca(pollDescriptorsCount * sizeof(struct pollfd));
+  snd_seq_poll_descriptors(hSequencer, pPollDescriptor, pollDescriptorsCount, POLLIN);
+
+  spdlog::trace("Alsa client {} created.", clientId);
 }
+
+std::atomic<bool> eventListening{false};
+/**
+ * Close the ALSA sequencer.
+ * This can only be called when event listening has stopped.
+ */
+void AlsaHelper::closeAlsaSequencer() {
+  // make sure the listening queue is stopped for sure.
+  if(eventListening){
+    throw std::runtime_error("Receiver cannot be stopped");
+  }
+
+  spdlog::trace("Closing Alsa client {}.", clientId);
+  int err = snd_seq_close 	(hSequencer);
+  checkAlsa("snd_seq_close", err);
+}
+
 
 /**
  * create an output (emitting) port.
@@ -65,7 +92,7 @@ void AlsaHelper::openAlsaSequencer() {
  */
 int AlsaHelper::createOutputPort(const char *portName) {
   int portId; /// the ID-number of our ALSA input port
-  portId = snd_seq_create_simple_port(_hSequencer, portName,
+  portId = snd_seq_create_simple_port(hSequencer, portName,
                                       SND_SEQ_PORT_CAP_READ | SND_SEQ_PORT_CAP_SUBS_READ,
                                       SND_SEQ_PORT_TYPE_APPLICATION);
   checkAlsa("createOutputPort", portId);
@@ -79,7 +106,7 @@ int AlsaHelper::createOutputPort(const char *portName) {
  */
 int AlsaHelper::createInputPort(const char *portName) {
   int portId; /// the ID-number of our ALSA input port
-  portId = snd_seq_create_simple_port(_hSequencer, portName,
+  portId = snd_seq_create_simple_port(hSequencer, portName,
                                       SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE,
                                       SND_SEQ_PORT_TYPE_APPLICATION);
   checkAlsa("createInputPort", portId);
@@ -96,16 +123,16 @@ void AlsaHelper::connectPorts(int hEmitterPort, int hReceiverPort) {
   snd_seq_addr_t emitter, receiver;
   snd_seq_port_subscribe_t *pSubscriptionDescription;
 
-  emitter.client = _clientId;
+  emitter.client = clientId;
   emitter.port = hEmitterPort;
 
-  receiver.client = _clientId;
+  receiver.client = clientId;
   receiver.port = hReceiverPort;
 
   snd_seq_port_subscribe_alloca(&pSubscriptionDescription);
   snd_seq_port_subscribe_set_sender(pSubscriptionDescription, &emitter);
   snd_seq_port_subscribe_set_dest(pSubscriptionDescription, &receiver);
-  auto err = snd_seq_subscribe_port(_hSequencer, pSubscriptionDescription);
+  auto err = snd_seq_subscribe_port(hSequencer, pSubscriptionDescription);
   checkAlsa("connectPorts", err);
 }
 /**
@@ -126,15 +153,64 @@ void AlsaHelper::sendEvents(int hEmitterPort, int eventCount, long intervalMs) {
 
   ev.type = SND_SEQ_EVENT_NOTEON;
   ev.data.note.channel = 1;
-
   ev.data.note.velocity = 0;
 
   for (int i = 0; i < eventCount; ++i) {
     ev.data.note.note = i%128;
-    snd_seq_event_output_direct(_hSequencer, &ev);
+    snd_seq_event_output_direct(hSequencer, &ev);
     std::this_thread::sleep_for(std::chrono::milliseconds(intervalMs));
   }
+}
 
+
+
+
+int AlsaHelper::retrieveEvents(){
+
+  snd_seq_event_t *ev;
+  int eventCount = 0;
+
+  while(snd_seq_event_input(hSequencer, &ev)>0){
+    spdlog::trace("AlsaHelper::retrieveEvents()");
+
+    switch (ev->type) {
+    case SND_SEQ_EVENT_NOTEON:
+      eventCount++;
+      break;
+    }
+    snd_seq_free_event(ev);
+  }
+  return eventCount;
+}
+
+int AlsaHelper::listenForEventsLoop(){
+  int eventCount = 0;
+  while(eventListening) {
+    auto hasEvents = poll(pPollDescriptor, pollDescriptorsCount, POLL_TIMEOUT_MS);
+    if (hasEvents > 0) {
+      eventCount = eventCount + retrieveEvents();
+    }
+  }
+  return eventCount;
+}
+
+FutureEventCount AlsaHelper::startEventReceiver() {
+  eventListening = true;
+  spdlog::trace("AlsaHelper::startEventReceiver()");
+  return std::async(std::launch::async, []() -> int {
+    return listenForEventsLoop();
+  });
+}
+
+void AlsaHelper::stopEventReceiver(FutureEventCount& future) {
+  spdlog::trace("AlsaHelper::stopEventReceiver()");
+  // stop the listenForEventsLoop
+  eventListening = false;
+  // wait until the FutureEventCount has become ready.
+  auto status = future.wait_for(std::chrono::milliseconds (2*POLL_TIMEOUT_MS));
+  if(status == std::future_status::timeout){
+    throw std::runtime_error("Receiver cannot be stopped");
+  }
 }
 
 } // namespace unit_test_helpers
