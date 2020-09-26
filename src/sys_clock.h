9026de246810a547f6beb0e06e523f2d4389479f
Harald-LB <Harald-lb@free-creations.de>
2020-09-26 11:30:09 +0200

    documentation added.

--- src/sys_clock.h
+++ src/sys_clock.h
@@ -22,21 +22,33 @@
 #include <chrono>
 
 /**
- * The module `sysClock`aims to simplify the use of the <chrono> Standard Library.
+ * THis module aims to simplify the use of the <chrono> Standard Library.
  */
 namespace sysClock {
 
-using SteadyClock = std::chrono::steady_clock;
-using TimePoint = SteadyClock::time_point;
-using SysTimeUnits = SteadyClock::duration;
-
+using Clockwork = std::chrono::high_resolution_clock;
+using TimePoint = Clockwork::time_point;
+using SysTimeUnits = Clockwork::duration;
+/**
+ * @return a time point representing the current point in time.
+ */
 inline TimePoint now(){
-  return SteadyClock::now();
+  return Clockwork::now();
 }
-
+/**
+ * Converts the given duration into a floating point number representing the duration
+ * in microseconds.
+ * @param duration - a duration in system time units.
+ * @return a floating point number representing the duration in microseconds.
+ */
 inline double toMicrosecondFloat(const SysTimeUnits& duration){
   return std::chrono::duration<double, std::micro>(duration).count();
 }
+/**
+ * Converts the given duration to system time-units.
+ * @param durationMicroseconds - duration in microseconds
+ * @return duration in system time-units.
+ */
 inline SysTimeUnits toSysTimeUnits(const float durationMicroseconds){
   auto asChronoUs = std::chrono::duration<float, std::micro>(durationMicroseconds);
   return std::chrono::duration_cast<SysTimeUnits>(asChronoUs);
